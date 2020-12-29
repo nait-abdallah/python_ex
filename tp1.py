bf9283e20517e8d587810f768dd0d2dd14ac83de
ABDALLAH NAIT ABDALLAH <naitabdallah.abdallah@gmail.com>
2020-12-29 20:04:39 +0100

    ex 5

--- tp1.py
+++ tp1.py
@@ -47,9 +47,12 @@ def ex4():
 
 #L'utilisateur donne un entier positif et le programme annonce combien de fois de suite cet entier est divisible par 2.
 def ex5():
-    a = None
-    try:
-        a = int(input("entrer un nombre entier : "))
-    except:
-        print("erreur de saisi !!! ")
-        
+    a = int(print("entrer un nombre"))
+    res = 0
+    while a !=0 :
+        if a % 2 == 0 :
+            res += 1
+        else:
+            break
+    return res            
+ex5()
